697e97fe6eb0029e26699b4f5f73ae951d1f497c
itcaituotuo <291151689@qq.com>
2021-01-11 11:45:11 +0800

    修改代码测试

--- case/web/cnblogs/index.py
+++ case/web/cnblogs/index.py
@@ -11,8 +11,8 @@ import warnings
 class BlogIndex(unittest.TestCase):
     def setUp(self):
         for i in range(0, 10000):
-            # # 忽略警告，Enable tracemalloc to get the object allocation traceback
-            # warnings.simplefilter("ignore", ResourceWarning)
+            # 忽略警告，Enable tracemalloc to get the object allocation traceback
+            warnings.simplefilter("ignore", ResourceWarning)
 
             # 实例化driver
             self.driver = webdriver.Chrome()
@@ -21,19 +21,19 @@ class BlogIndex(unittest.TestCase):
             self.driver.get("https://www.cnblogs.com/caituotuo/p/14253800.html")
             # 窗口最大化
             self.driver.maximize_window()
-            time.sleep(10)
+            time.sleep(60)
 
             # 软件测试基础知识
             self.driver.get("https://www.cnblogs.com/caituotuo/p/14226452.html")
             # 窗口最大化
             self.driver.maximize_window()
-            time.sleep(10)
+            time.sleep(60)
 
             # 接口自动化测试
             self.driver.get("https://www.cnblogs.com/caituotuo/p/14260258.html")
             # 窗口最大化
             self.driver.maximize_window()
-            time.sleep(10)
+            time.sleep(60)
             self.driver.quit()
 
     def tearDown(self):
